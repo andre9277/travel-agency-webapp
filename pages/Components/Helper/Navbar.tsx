2263d185f64ccf3bf1b519fbc382ff1cacea73cf
André Ferreira <110822583+andre9277@users.noreply.github.com>
2024-02-05 15:33:28 +0000

    Added "Book Now" button & icon mobile
    Added the "Book Now" button to the navbar.
    Added also the navbar icon for mobile users.

--- pages/Components/Helper/Navbar.tsx
+++ pages/Components/Helper/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { Bars3Icon } from "@heroicons/react/16/solid";
 
 const Navbar = () => {
   return (
@@ -24,6 +25,15 @@ const Navbar = () => {
           <a href="#" className="nav-link">
             Contact
           </a>
+          <a
+            href="#"
+            className="relative hidden lg:inline-flex items-center justify-center px-10 py-3 overflow-hidden font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
+          >
+            <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-600 rounded-full group-hover:w-56 group-hover:h-56"></span>
+            <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-t from-transparent via-transparent to-gray-700"></span>
+            <span className="relative"> Book Now</span>
+          </a>
+          <Bars3Icon className="w-[2rem] lg:hidden h-[2rem] text-blue-800 font-bold" />
         </div>
       </div>
     </div>
