1722c6a0e4c4b91393136f7bfaf311d74be035e8
André Ferreira <110822583+andre9277@users.noreply.github.com>
2024-02-05 15:19:45 +0000

    Added Navbar options
    Added the navbar options with style.
    Added Home, Tour, hotel, review and contact navbar

--- pages/Components/Helper/Navbar.tsx
+++ pages/Components/Helper/Navbar.tsx
@@ -5,8 +5,25 @@ const Navbar = () => {
   return (
     <div className="w-[100%] bg-white">
       <div className="flex w-[80%] mx-auto items-center justify-between h-[12vh">
-        <div className="relative w-[70px] h-[70px] lg:w-[100px] lg:h-[100px] object-contain">
+        <div className="relative w-[70px] h-[70px] cursor-pointer lg:w-[100px] lg:h-[100px] object-contain">
           <Image src="/images/logo.png" alt="logo" layout="fill" />
+        </div>
+        <div className="flex items-center space-x-12">
+          <a href="#" className="nav-link">
+            Home
+          </a>
+          <a href="#" className="nav-link">
+            Tour
+          </a>
+          <a href="#" className="nav-link">
+            Hotel
+          </a>
+          <a href="#" className="nav-link">
+            Review
+          </a>
+          <a href="#" className="nav-link">
+            Contact
+          </a>
         </div>
       </div>
     </div>
